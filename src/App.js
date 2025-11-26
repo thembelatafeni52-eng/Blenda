3a384daf9b93b4aa67047f1bf3c59e7ae0f51087
thembelatafeni52-eng <thembelatafeni52@gmail.com>
2025-11-27 00:05:28 +0200

    Create initial App component with welcome message

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Welcome to Blenda 🚀</h1>
+      <p>Your app setup is working perfectly.</p>
+    </div>
+  );
+}
