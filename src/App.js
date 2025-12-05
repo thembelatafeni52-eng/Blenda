2a36c000cbb1f1552f573c02b960230d4b0e8d8f
thembelatafeni52-eng <thembelatafeni52@gmail.com>
2025-12-05 07:33:07 +0200

    Refactor App component to use AppNavigator

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import Dashboard from "./components/Dashboard";
-import "./App.css";
+import AppNavigator from "./navigation/AppNavigator";
+import "./styles/theme.css";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <AppNavigator />
+    </div>
   );
 }
 
