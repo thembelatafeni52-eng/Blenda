6387bf7fffe981129c45aa6641655d95d918fb2b
thembelatafeni52-eng <thembelatafeni52@gmail.com>
2025-12-09 01:09:20 +0200

    Update Footer.jsx

--- src/ components/Footer.jsx
+++ src/ components/Footer.jsx
@@ -1,1 +1,17 @@
+export default function Footer() {
+  return (
+    <footer style={styles.footer}>
+      <p>© {new Date().getFullYear()} Blenda — Have a bright future</p>
+    </footer>
+  );
+}
 
+const styles = {
+  footer: {
+    textAlign: "center",
+    backgroundColor: "#e7e7e7",
+    padding: "20px",
+    marginTop: "40px",
+    fontSize: "16px",
+  },
+};
