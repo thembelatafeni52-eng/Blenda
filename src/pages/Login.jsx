b3889ce28abc5645fec31df59d62257c87bd1030
thembelatafeni52-eng <thembelatafeni52@gmail.com>
2025-12-09 01:11:46 +0200

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,1 +1,32 @@
+export default function Login() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>Login</h1>
 
+      <input type="email" placeholder="Email" style={styles.input} />
+      <input type="password" placeholder="Password" style={styles.input} />
+
+      <button style={styles.btn}>Login</button>
+    </div>
+  );
+}
+
+const styles = {
+  input: {
+    width: "80%",
+    padding: "15px",
+    margin: "15px auto",
+    display: "block",
+    borderRadius: "8px",
+    border: "1px solid #ccc",
+  },
+  btn: {
+    padding: "12px 30px",
+    background: "#0A4DFF",
+    color: "#fff",
+    borderRadius: "8px",
+    border: "none",
+    cursor: "pointer",
+    marginTop: "15px",
+  },
+};
