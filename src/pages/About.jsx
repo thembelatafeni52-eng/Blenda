a822dc3cfe8d5abd7e41491f2da497fd8d4bb6d0
thembelatafeni52-eng <thembelatafeni52@gmail.com>
2025-12-09 01:10:12 +0200

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,1 +1,12 @@
-
+export default function About() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>About Blenda</h1>
+      <p style={{ fontSize: "20px", marginTop: "20px" }}>
+        Blenda is a modern platform built to empower, inspire, and create 
+        a brighter future.  
+        We combine technology and design to deliver meaningful experiences.
+      </p>
+    </div>
+  );
+}
