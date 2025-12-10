736a4b5b9c736a0bd55b288db433595ab178ffa6
thembelatafeni52-eng <thembelatafeni52@gmail.com>
2025-12-10 21:34:09 +0200

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,15 +1,12 @@
 import React from "react";
 
-export default function Contact() {
+function Contact() {
   return (
-    <div className="container">
+    <div>
       <h1>Contact Us</h1>
-      <p>Need support? We're here to help.</p>
-
-      <ul>
-        <li>Email: support@blenda.ai</li>
-        <li>Telegram: @blenda_support</li>
-      </ul>
+      <p>Email: support@blenda.ai</p>
     </div>
   );
 }
+
+export default Contact;
