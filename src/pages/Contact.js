275f54fffbc15dd88678c7f2cb9fbac95d1a429d
thembelatafeni52-eng <thembelatafeni52@gmail.com>
2025-12-10 19:25:59 +0200

    Create Contact.js

---
+++ src/pages/Contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <div className="container">
+      <h1>Contact Us</h1>
+      <p>Need support? We're here to help.</p>
+
+      <ul>
+        <li>Email: support@blenda.ai</li>
+        <li>Telegram: @blenda_support</li>
+      </ul>
+    </div>
+  );
+}
