c35e643cc8c2c94c3a4b57fbe43b4cfe7f0154bc
thembelatafeni52-eng <thembelatafeni52@gmail.com>
2025-12-10 19:31:53 +0200

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,1 +1,52 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    // This will be replaced with Firebase later
+    console.log("Login:", email, password);
+
+    alert("Logged in successfully!");
+  };
+
+  return (
+    <div style={{ padding: "40px" }}>
+      <h2>Login</h2>
+
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>Email:</label><br />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+
+        <div style={{ marginTop: "10px" }}>
+          <label>Password:</label><br />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+
+        <button style={{ marginTop: "20px" }}>Login</button>
+      </form>
+
+      <p style={{ marginTop: "10px" }}>
+        Don't have an account? <Link to="/register">Register</Link>
+      </p>
+    </div>
+  );
+}
+
+export default Login;
