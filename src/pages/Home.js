5b50897efe9e2c2b38fd4b538795cd7207e0d8a7
thembelatafeni52-eng <thembelatafeni52@gmail.com>
2025-12-10 21:31:30 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div style={{ padding: "40px", textAlign: "center" }}>
-      <h1>Welcome to Blenda</h1>
-      <p>Your smart trading assistant.</p>
-
-      <div style={{ marginTop: "30px" }}>
-        <Link to="/login" style={{ marginRight: "20px" }}>
-          Login
-        </Link>
-        <Link to="/register">Register</Link>
-      </div>
+    <div>
+      <h1>Welcome to Blenda Trading App</h1>
+      <p>This is your homepage.</p>
     </div>
   );
 }
