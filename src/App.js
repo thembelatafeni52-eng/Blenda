d9d7fcd450ea70a7b34bb30fe8f8778b1215240b
thembelatafeni52-eng <thembelatafeni52@gmail.com>
2025-12-10 19:28:44 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
-console.log("App started");
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
