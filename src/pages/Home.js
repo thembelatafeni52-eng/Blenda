edaab265c62e0def4ddbbd4e03bb5ad8fb28d07e
thembelatafeni52-eng <thembelatafeni52@gmail.com>
2025-12-10 19:26:42 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,1 +1,10 @@
+import React from "react";
 
+export default function Home() {
+  return (
+    <div className="container">
+      <h1>Blenda Dashboard</h1>
+      <p>Your smart trading assistant.</p>
+    </div>
+  );
+}
